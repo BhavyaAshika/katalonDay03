34ae5fdabe9747ffc5f232deea4d66f1c5aec94f
GR644HV <GR644HV@XW5CG3415Q46.ey.net>
2024-09-28 18:06:53 +0530

    Test suite

---
+++ Test Suites/Enter Store.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Enter Store</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6c8c95c4-b8b2-42c9-b34e-0f05b04f839e</testSuiteGuid>
+   <testCaseLink>
+      <guid>294f6785-d654-45b5-9f85-249571b7e244</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Enter Store</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
